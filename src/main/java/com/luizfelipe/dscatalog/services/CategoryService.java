da9ffb3c5b6e5411f7804b6727d9c94f9610e95f
Luiz Felipe <luizfelipe01.2007@gmail.com>
2025-11-30 02:30:04 -0300

    CategoryService, CategoryRepository and database seeding

---
+++ src/main/java/com/luizfelipe/dscatalog/services/CategoryService.java
@@ -0,0 +1,21 @@
+package com.luizfelipe.dscatalog.services;
+
+import com.luizfelipe.dscatalog.entities.Category;
+import com.luizfelipe.dscatalog.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository repository;
+
+    @Transactional(readOnly = true)
+    public List<Category> findAll() {
+        return repository.findAll();
+    }
+}
